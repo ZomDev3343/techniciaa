1eadb3814cdcdea8aad1a03186c766867e96a452
Zom <contact.zomzd@gmail.com>
2022-05-13 16:46:25 +0200

    ADD : Omni Tool 2/? -> WIP : Durability is working

--- src/main/java/fr/zom/technicia/Technicia.java
+++ src/main/java/fr/zom/technicia/Technicia.java
@@ -7,13 +7,19 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(Technicia.MODID)
 public class Technicia {
 
     public static final String MODID = "technicia";
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
     public Technicia() {
+
+
 
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
@@ -26,17 +32,23 @@ public class Technicia {
 
     public void setup(final FMLCommonSetupEvent e)
     {
+        LOGGER.info("SETUP START");
         IEventBus eventBus = MinecraftForge.EVENT_BUS;
+        LOGGER.info("SETUP COMPLETE");
     }
 
     public void clientSetup(final FMLClientSetupEvent e)
     {
+        LOGGER.info("CLIENT SETUP START");
+        LOGGER.info("CLIENT SETUP COMPLETE");
 
     }
 
     private void registerAll(IEventBus bus)
     {
+        LOGGER.info("REGISTRIES...");
         ModItems.ITEMS.register(bus);
+        LOGGER.info("REGISTRIES DONE");
     }
 
 }
--- src/main/java/fr/zom/technicia/items/ItemOmniTool.java
+++ src/main/java/fr/zom/technicia/items/ItemOmniTool.java
@@ -2,12 +2,15 @@ package fr.zom.technicia.items;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import fr.zom.technicia.Technicia;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -15,14 +18,20 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
+
+import java.util.Set;
+import java.util.function.Consumer;
 
 public class ItemOmniTool extends Item {
 
     // TODO Modèle dynamique en fonction du tier
-    // TODO -> en fonction de l'état aussi
+    // TODO -> en fonction de l'état (cassé) aussi
     // TODO Modèle 3D
     // TODO Modèle JSON
     /* TODO
@@ -35,34 +44,65 @@ public class ItemOmniTool extends Item {
      */
     // TODO Ajouter une description avec les stats
     // TODO Modifier le nom pour chaque tier
-    // TODO Corriger le bug des dégats
     // TODO Vérifier la durabilité
+    // TODO Nettoyer et optimiser le code
+    // TODO Afficher quand l'outil est cassé
+
+    public static final Set<ToolAction> ALWAYS_SUPPORTED_ACTIONS = Set.of(ToolActions.AXE_DIG, ToolActions.SHOVEL_DIG, ToolActions.PICKAXE_DIG,
+            ToolActions.SWORD_DIG);
 
     public ItemOmniTool() {
-        super(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON).fireResistant().setNoRepair());
+        super(new Item.Properties().rarity(Rarity.UNCOMMON).fireResistant().setNoRepair().durability(10));
     }
 
     @Override
-    public boolean isEnchantable(ItemStack pStack) {
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
         return false;
     }
 
     @Override
-    public boolean isDamageable(ItemStack stack) {
-        boolean damageable = stack.getDamageValue() != 1;
+    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+        return false;
+    }
 
-        if (!damageable) {
-            CompoundTag tag = stack.getOrCreateTag();
-            tag.putBoolean("broken", true);
+    @Override
+    public boolean canPerformAction(ItemStack stack, ToolAction toolAction) {
+        return ALWAYS_SUPPORTED_ACTIONS.contains(toolAction);
+    }
+
+    @Override
+    public boolean mineBlock(ItemStack pStack, Level pLevel, BlockState pState, BlockPos pPos, LivingEntity pMiningEntity) {
+
+        if(pStack.getOrCreateTag().getBoolean("broken"))
+            return false;
+
+        if (!pLevel.isClientSide && pState.getDestroySpeed(pLevel, pPos) != 0.0F) {
+
+            pStack.hurtAndBreak(1, pMiningEntity, (p_40992_) -> {
+                p_40992_.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+            });
+
         }
+        return true;
+    }
 
-        return damageable;
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+
+        if(pStack.getOrCreateTag().getBoolean("broken"))
+            return false;
+
+        pStack.hurtAndBreak(2, pAttacker, (p_41007_) -> {
+            p_41007_.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+        });
+        return true;
     }
 
     @Override
     public float getDestroySpeed(ItemStack pStack, BlockState pState) {
         CompoundTag tag = pStack.getOrCreateTag();
-        if(tag.getBoolean("broken"))
+        boolean isBroken = tag.getBoolean("broken");
+        if (isBroken)
             return 0f;
         return isCorrectTier(pStack, pState) ? tag.getFloat("mineSpd") : 1f;
     }
@@ -87,12 +127,13 @@ public class ItemOmniTool extends Item {
         CompoundTag tag = stack.getOrCreateTag();
 
         int tier = tag.getInt("tier");
-        int blockTier = -1;
+        int blockTier = 0;
 
-        if(state.is(BlockTags.NEEDS_STONE_TOOL)) blockTier = 0;
-        if(state.is(BlockTags.NEEDS_IRON_TOOL)) blockTier = 1;
-        if(state.is(BlockTags.NEEDS_DIAMOND_TOOL)) blockTier = 2;
-        if(state.is(Tags.Blocks.NEEDS_NETHERITE_TOOL)) blockTier = 3;
+        if (state.is(BlockTags.NEEDS_IRON_TOOL)) blockTier = 1;
+        if (state.is(BlockTags.NEEDS_DIAMOND_TOOL)) blockTier = 2;
+        if (state.is(Tags.Blocks.NEEDS_NETHERITE_TOOL)) blockTier = 3;
+
+        Technicia.LOGGER.debug("Block : {}, Tier : {}", state.getBlock().getRegistryName(), blockTier);
 
         return switch (tier) {
             case 0, 1 -> blockTier < 1;
@@ -104,10 +145,30 @@ public class ItemOmniTool extends Item {
     }
 
     @Override
+    public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, T entity, Consumer<T> onBroken) {
+
+        boolean broken = true;
+        if(stack.getDamageValue() + amount == stack.getMaxDamage())
+        {
+            stack.getOrCreateTag().putBoolean("broken", true);
+            stack.setDamageValue(0);
+        }else
+        {
+            broken = false;
+        }
+
+        return broken ? 0 : amount;
+    }
+
+    @Override
     public int getMaxDamage(ItemStack stack) {
         CompoundTag tag = stack.getOrCreateTag();
-        return tag.contains("durability") ? tag.getInt("durability") : 10;
+
+        int durability = tag.getInt("durability");
+
+        return durability;
     }
+
 
     @Override
     public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
@@ -133,8 +194,7 @@ public class ItemOmniTool extends Item {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        if(!pLevel.isClientSide)
-        {
+        if (!pLevel.isClientSide) {
             ItemStack stack = pPlayer.getItemInHand(pUsedHand);
             nextTier(stack);
             return InteractionResultHolder.success(stack);
@@ -143,11 +203,10 @@ public class ItemOmniTool extends Item {
         return InteractionResultHolder.fail(pPlayer.getItemInHand(pUsedHand));
     }
 
-    private void nextTier(ItemStack stack)
-    {
+    private void nextTier(ItemStack stack) {
         CompoundTag tag = stack.getOrCreateTag();
         int currentTier = tag.getInt("tier");
-        if(currentTier == 5)
+        if (currentTier == 5)
             currentTier = 0;
         else ++currentTier;
         changeTier(stack, OmniToolTier.getTier(currentTier));
@@ -157,12 +216,17 @@ public class ItemOmniTool extends Item {
     public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
 
         CompoundTag tag = stack.getOrCreateTag();
+        boolean broken = tag.getBoolean("broken");
 
-        float atkDmg = tag.getBoolean("broken") ? tag.getFloat("atkDmg") : 0f;
+        if (!broken) {
+            float atkDmg = tag.getFloat("atkDmg");
 
-        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", atkDmg, AttributeModifier.Operation.ADDITION));
-        return slot == EquipmentSlot.MAINHAND ? builder.build() : ImmutableMultimap.of();
+            ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+            builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", atkDmg, AttributeModifier.Operation.ADDITION));
+            return slot == EquipmentSlot.MAINHAND ? builder.build() : ImmutableMultimap.of();
+        }
+
+        return ImmutableMultimap.of();
     }
 
     public static void changeTier(ItemStack stack, OmniToolTier tier) {
@@ -171,8 +235,13 @@ public class ItemOmniTool extends Item {
         tag.putFloat("mineSpd", tier.getMiningSpeed());
         tag.putFloat("atkDmg", tier.getAttackDamage());
         tag.putInt("tier", tier.getTier());
+        tag.putInt("durability", tier.getMaxDurability());
+        if(stack.getDamageValue() < tier.getMaxDurability())
+            stack.getOrCreateTag().putBoolean("broken", false);
+        else
+            stack.getOrCreateTag().putBoolean("broken", true);
 
-        System.out.println("Changement de tier !");
+        Technicia.LOGGER.debug("Changement de tier Omni Tool {}", tier.getTier());
     }
 
     private static void initTier(ItemStack stack) {
@@ -182,12 +251,12 @@ public class ItemOmniTool extends Item {
 
     public enum OmniToolTier {
 
-        WEAK(3f, 1f, 250, 0),
+        WEAK(3f, 1f, 5, 0),
         BASIC(5f, 2f, 500, 1),
         STRONG(7f, 4f, 1024, 2),
         ADVANCED(9f, 5f, 2048, 3),
-        ENHANCED(11f, 7f, 4096, 4),
-        ULTIMATE(13f, 9f, 8192, 5);
+        ENHANCED(13f, 7f, 4096, 4),
+        ULTIMATE(99f, 9f, 8192, 5);
 
         private final float miningSpeed;
         private final float attackDamage;
